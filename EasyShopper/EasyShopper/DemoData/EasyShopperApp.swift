8b411429c99c4532dce4cf7cff664780cd03c22d
Matt Bonney <bonneym@gmail.com>
2023-02-01 17:50:15 -0500

    Move to SwiftUI App lifecycle

---
+++ EasyShopper/EasyShopper/DemoData/EasyShopperApp.swift
@@ -0,0 +1,18 @@
+//
+//  EasyShopperApp.swift
+//  EasyShopper
+//
+//  Created by Matt Bonney on 2/1/23.
+//  Copyright © 2023 Ka-ching. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct EasyShopper_App: App {
+    var body: some Scene {
+        WindowGroup {
+            SwiftUIEntryView()
+        }
+    }
+}
