3b12d3d7f3d21d74365ba468558a362cda8be6b1
Matt Bonney <bonneym@gmail.com>
2023-02-01 17:53:57 -0500

    Network service for handling urlsession requests and nothing more

--- EasyShopper/EasyShopper/Services/NetworkService.swift
+++ EasyShopper/EasyShopper/Services/NetworkService.swift
@@ -11,11 +11,10 @@ import Foundation
 
 /// A service responsible for network activity.
 class NetworkService {
-    /// The particular URL this service will interact with.
-    private var endpoint: URL
-
-    init(endpoint: URL) {
-        self.endpoint = endpoint
+    private let urlSession: URLSession
+    
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
     }
 }
 
@@ -24,20 +23,16 @@ extension NetworkService {
     func data(for url: URL) async -> Result<Data, Error> {
         do {
             /// Run the async URLSession request.
-            let (data, _) = try await URLSession.shared.data(from: url)
+            let (data, _) = try await self.urlSession.data(from: url)
             /// If we got data in the `try` request, return the data.
             return .success(data)
         } catch (let error) {
             /// Error case: something went wrong with the URLRequest.
             /// Log the error. In production, I'd use an `os.Logger` at the bare minimum.
-            print("🌐 Error in NetworkService: \(error.localizedDescription)")
+            print("🌐 Error in NetworkService.")
+            print(error)
             /// Return a failure result with the error.
             return .failure(error)
         }
     }
-}
-
-extension NetworkService {
-    /// Endpoint URL for this practice project.
-    static let endpoint = URL(string: "https://run.mocky.io/v3/4e23865c-b464-4259-83a3-061aaee400ba")!
 }
